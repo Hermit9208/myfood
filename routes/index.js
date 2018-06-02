2d9bcfc019d26f56d4b592d2b52b197b19316792
Hermit9208 <Hermit9208@gmail.com>
2018-06-02 16:30:03 +0600

    myfood

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+var orders = []
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', {orders});
+});
+
+ router.get('/form', function(req, res, next) {
+ 	//console.log(req.query)
+   res.render('form', {});
+ });
+
+router.post('/form', function(req, res, next) {
+orders.push({
+	food: req.body.foodName,
+	name: req.body.clients
+})
+
+  res.render('form', {});
+});
+
+module.exports = router;
